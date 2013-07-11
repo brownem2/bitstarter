469ee6bf971cd73b98bfecdcc43ae865fc25b6f3
gaviriar <ricardo.a.gaviria@gmail.com>
2013-07-11 04:55:07 +0000

    modified  web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-buff  = fs.readFileSync(index.html);
+buff  = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
 response.send(buff.toString());
